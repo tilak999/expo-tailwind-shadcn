6bd397230857d981e769639189fade007c85ac5a
Tilak Sasmal <tilaksasmal999@gmail.com>
2024-03-07 08:04:31 +0000

    Milestone 01: Got everything working

--- expo-router/app/(tabs)/two.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function TabTwoScreen() {
-  return (
-    <View>
-      <Text className="text-black-400">Tab two!!</Text>
-    </View>
-  );
-}
